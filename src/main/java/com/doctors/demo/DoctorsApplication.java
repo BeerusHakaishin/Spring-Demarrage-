47d98d7d91336d20eb61dbeb19319974ea6f1fb1
Ismaël Meziani <meziani-i@outlook.fr>
2021-12-20 10:38:41 +0100

    base quest

---
+++ src/main/java/com/doctors/demo/DoctorsApplication.java
@@ -0,0 +1,50 @@
+package com.doctors.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class DoctorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorsApplication.class, args);
+	}
+	
+@RequestMapping("/doctor/1")
+	@ResponseBody
+	public String doctor1() {
+		return "William Hartnell";
+	}
+	
+@RequestMapping("/doctor/7")
+@ResponseBody
+public String doctor7() {
+	return "Sylvester McCoy";
+}
+
+@RequestMapping("/doctor/9")
+@ResponseBody
+public String doctor9() {
+	return "Christopher Eccleston";
+}
+
+@RequestMapping("/doctor/13")
+@ResponseBody
+public String doctor13() {
+	return "Jodie Whittaker";
+}
+
+@RequestMapping("/")
+@ResponseBody
+public String index() {
+	return "<ul><li><a href='/doctor/1'>First doctor</a></li>" +
+			"<li><a href='/doctor/7'>Second doctor</a></li>" +
+			"<li><a href='/doctor/9'>Third doctor</a></li>" +
+			"<li><a href='/doctor/13'>Thirteenth doctor</a></li>";
+}
+
+}
---
+++ src/test/java/com/doctors/demo/DoctorsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doctors.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
